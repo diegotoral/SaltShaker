edc33eeb769398113a99de7f994bd13a497e9161
Diego Toral <diegotoral@gmail.com>
2014-12-18 22:55:45 -0300

    Update setup.py with keywords and long description

--- setup.py
+++ setup.py
@@ -2,12 +2,20 @@
 
 from setuptools import setup
 
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
 setup(name='saltshaker',
       version='0.0.1',
       description='Development environment provisioning with Salt made easy. Just Shaker it!',
+      long_description=readme(),
       url='https://github.com/diegotoral/SaltShaker',
       author='Diego Toral',
       author_email='diegotoral@gmail.com',
       license='MIT',
       packages=['saltshaker'],
+      keywords='saltshaker salt provisioning',
       zip_safe=False)
